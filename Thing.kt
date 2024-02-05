b1d6855d9d40c5c13d28d6c274a76fb37b797293
PopBS <147734677+PopBS@users.noreply.github.com>
2024-02-05 14:55:53 +0000

    Update Thing.kt

--- Thing.kt
+++ Thing.kt
@@ -1,43 +1,42 @@
-package com.example.birthdaycard
+package com.example.greetingcard
 
-import androidx.compose.foundation.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.ui.graphics.Color
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.birthdaycard.ui.theme.BirthdayCardTheme
+import com.example.greetingcard.ui.theme.GreetingCardTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BirthdayCardTheme {
+            GreetingCardTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    GreetingImage(message = "HAppy britdya to uuU",
-                        from = "frum the goat albeet"
-                    )
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    GreetingImage(message = "Albeet Popu", from = "The Android Developer")
+                    Contacts(number = "353891609522", email = "PopBS@protonmail.com")
+                    Icons()
                 }
             }
         }
@@ -45,75 +44,134 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
-    Column(
-        verticalArrangement = Arrangement.Center,
-        modifier = modifier
-    ) {
-        Text(
-            text = message,
-            fontSize = 30.sp,
-            lineHeight = 35.sp,
-            color = Color.Green,
-            textAlign = TextAlign.Center,
-            modifier = Modifier
-                .padding(14.dp)
-                .then(modifier)
-        )
-        Text(
-            text = from,
-            fontSize = 19.sp,
-            lineHeight = 10.sp,
-            modifier = Modifier
-                .padding(10.dp)
-                .align(alignment = Alignment.End)
-        )
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Surface(color = Color.Blue) {
+        Text(text = "Hi, my name is $name!", modifier = modifier.padding(14.dp))
     }
 }
 
 @Composable
 fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
-    val image = painterResource(R.drawable.androidparty)
+    val hexColor = "#3DDC84"
+    val TAD = "#026440"
+    val android_dude = painterResource(R.drawable.android_logo)
     Box(modifier = modifier) {
-        Image(
-            painter = image,
-            contentDescription = null,
-            contentScale = ContentScale.Crop ,
-            alpha = 0.7F
-
-        )
-        Text(
-            text = message,
-            fontSize = 70.sp,
-            color = Color.Green,
-            lineHeight = 80.sp,
-            textAlign = TextAlign.Center,
+        Column(
             modifier = Modifier
-                .padding(60.dp)
-                .align(alignment = Alignment.Center)
+                .fillMaxSize()
+                .background(Color(android.graphics.Color.parseColor(hexColor)))
+        ) {}
 
-        )
-        Text(
-            text = from,
-            fontSize = 19.sp,
-            lineHeight = 10.sp,
+        Column (
             modifier = Modifier
-                .padding(
-                    start = 100.dp,
-                    top = 600.dp,
-                    end = 16.dp,
-                    bottom = 16.dp,
-                )
-        )
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = android_dude,
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .offset(
+                        0.dp, 180.dp
+                    )
+                    .size(120.dp)
+                    .background(Color.Black)
+
+            )
+            Text(
+                text = message,
+                fontSize = 42.sp,
+                lineHeight = 90.sp,
+                color = Color(android.graphics.Color.parseColor(TAD)),
+                modifier = Modifier
+                    .offset(
+                        0.dp, 180.dp
+                    )
+            )
+            Text(
+                text = from,
+                fontSize = 19.sp,
+                lineHeight = 30.sp,
+                modifier = Modifier
+                    .offset(
+                        0.dp, 180.dp)
+            )
+        }
+        
+    }
+}
+
+@Composable
+fun Contacts(number: String, email: String, modifier: Modifier = Modifier) {
+    Box(modifier = Modifier) {
+        Column (
+            modifier = Modifier
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = number,
+                fontSize = 21.sp,
+                lineHeight = 30.sp,
+                modifier = Modifier
+                    .offset(
+                        0.dp, 480.dp)
+            )
+            Text(
+                text = email,
+                fontSize = 21.sp,
+                lineHeight = 30.sp,
+                modifier = Modifier
+                    .offset(
+                        0.dp, 480.dp)
+            )
+        }
+    }
+
+}
+
+
+@Composable
+fun Icons(modifier: Modifier = Modifier) {
+    val email_image = painterResource(R.drawable.ic_email)
+    val phone_image = painterResource(R.drawable.ic_phone)
+    Box(modifier = Modifier) {
+        Column (
+            modifier = Modifier
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = phone_image,
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .offset(
+                        -95.dp, 481.dp
+                    )
+                    .size(27.dp)
+            )
+            Image(
+                painter = email_image,
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .offset(
+                        -140.dp, 483.dp
+                    )
+                    .size(27.dp)
+            )
+        }
     }
 }
 
 
 
-@Preview(showBackground = false)
+@Preview(showBackground = true)
 @Composable
-fun BirthdayCardPreview() {
-    BirthdayCardTheme {
-        GreetingImage(message = "HAppy britdya", from = "frum the goat albeet")
+fun GreetingPreview() {
+    GreetingCardTheme {
+        Greeting("Albert")
     }
 }
